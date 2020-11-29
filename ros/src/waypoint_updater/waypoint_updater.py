4d9ddc4e6fd75a6cf94fe2052743c75951c2bc97
root <marcos.sob@hotmail.com>
2020-11-29 09:37:57 +0000

    remove prints

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -30,7 +30,6 @@ LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this n
 class WaypointUpdater(object):
     def __init__(self):
         rospy.init_node('waypoint_updater')
-        print("test")
 
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
@@ -49,11 +48,9 @@ class WaypointUpdater(object):
         self.loop()
     
     def loop(self):
-        print("inside loop")
         rate = rospy.Rate(50)
         while not rospy.is_shutdown():
             if self.pose and self.waypoint_tree:
-                print("inside cond")
                 closest_idx = self.get_closest_waypoint_idx()
                 self.publish_waypoints(closest_idx)
             rate.sleep()              
